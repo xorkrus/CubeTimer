ddb115c73302e07548be25e8a1241b37cc86f1dc
xorkrus <2152104+xorkrus@users.noreply.github.com>
2026-02-26 12:40:46 +0300

    Update User_Setup.h

--- configs/User_Setup.h
+++ configs/User_Setup.h
@@ -1,1 +1,11 @@
-
+#define TFT_CS  17
+#define TFT_DC  20
+#define TFT_RST 21
+#define TFT_MOSI 19
+#define TFT_SCLK 18
+#define SPI_FREQUENCY  40000000
+#define SPI_READ_FREQUENCY  20000000
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 240
+#define GC9A01_DRIVER
+#define USE_HSPI_PORT  // если нужно использовать аппаратный SPI
